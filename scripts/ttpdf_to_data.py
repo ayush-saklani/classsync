4e71f28094312a6eeebea69d6f84d231f1466ecc
RawatDevanshu <rawatdevanshu22@gmail.com>
2024-03-26 22:08:19 +0530

    added teacher name and class_id field to output json
    created subject to faculty dict in restructure.json to get teacher name
    renamed some classnames in class_to_id dict file
    renamed source file for ttpdf_to_data.py

--- scripts/ttpdf_to_data.py
+++ scripts/ttpdf_to_data.py
@@ -1,6 +1,6 @@
 import camelot
 
-tables = camelot.read_pdf(filepath="../asset/docs/alltimetable.pdf")
+tables = camelot.read_pdf(filepath="../asset/docs/timetable project.pdf")
 tables.export("output.json", f="json")
 # tables.to_json("foo.json")
 print(tables)
