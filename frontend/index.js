f8e7ddcd45638c595084890b5a4f0d8ed159faf9
“ayush <ayushsaklani04@gmail.com>
2024-03-27 17:13:17 +0530

    timetable viewer progress 70%

--- frontend/index.js
+++ frontend/index.js
@@ -1,37 +1,140 @@
+let temp_obj = {
+    "course": "",
+    "semester": 0,
+    "section": "",
+    "schedule": {
+      "mon": {
+        "08-09": { "class_id": "lt204", "slotdata": "mon", "teacher_ID": 0 },
+        "09-10": { "class_id": "", "slotdata": "", "teacher_ID": 0 },
+        "10-11": { "class_id": "lt204", "slotdata": "mon", "teacher_ID": 0 },
+        "11-12": { "class_id": "lt204", "slotdata": "mon", "teacher_ID": 0 },
+        "12-01": { "class_id": "lt204", "slotdata": "mon", "teacher_ID": 0 },
+        "01-02": { "class_id": "lt204", "slotdata": "mon", "teacher_ID": 0 },
+        "02-03": { "class_id": "lt204", "slotdata": "mon", "teacher_ID": 0 },
+        "03-04": { "class_id": "lt204", "slotdata": "mon", "teacher_ID": 0 },
+        "04-05": { "class_id": "lt204", "slotdata": "mon", "teacher_ID": 0 },
+        "05-06": { "class_id": "lt204", "slotdata": "mon", "teacher_ID": 0 }
+      },
+      "tue": {
+        "08-09": { "class_id": "lt204", "slotdata": "tue", "teacher_ID": 0 },
+        "09-10": { "class_id": "", "slotdata": "", "teacher_ID": 0 },
+        "10-11": { "class_id": "lt204", "slotdata": "tue", "teacher_ID": 0 },
+        "11-12": { "class_id": "lt204", "slotdata": "tue", "teacher_ID": 0 },
+        "12-01": { "class_id": "lt204", "slotdata": "tue", "teacher_ID": 0 },
+        "01-02": { "class_id": "lt204", "slotdata": "tue", "teacher_ID": 0 },
+        "02-03": { "class_id": "lt204", "slotdata": "tue", "teacher_ID": 0 },
+        "03-04": { "class_id": "lt204", "slotdata": "tue", "teacher_ID": 0 },
+        "04-05": { "class_id": "lt204", "slotdata": "tue", "teacher_ID": 0 },
+        "05-06": { "class_id": "lt204", "slotdata": "tue", "teacher_ID": 0 }
+      },
+      "wed": {
+        "08-09": { "class_id": "lt204", "slotdata": "web", "teacher_ID": 0 },
+        "09-10": { "class_id": "lt204", "slotdata": "web", "teacher_ID": 0 },
+        "10-11": { "class_id": "lt204", "slotdata": "web", "teacher_ID": 0 },
+        "11-12": { "class_id": "lt204", "slotdata": "web", "teacher_ID": 0 },
+        "12-01": { "class_id": "lt204", "slotdata": "web", "teacher_ID": 0 },
+        "01-02": { "class_id": "lt204", "slotdata": "web", "teacher_ID": 0 },
+        "02-03": { "class_id": "lt204", "slotdata": "web", "teacher_ID": 0 },
+        "03-04": { "class_id": "lt204", "slotdata": "web", "teacher_ID": 0 },
+        "04-05": { "class_id": "lt204", "slotdata": "web", "teacher_ID": 0 },
+        "05-06": { "class_id": "lt204", "slotdata": "web", "teacher_ID": 0 }
+      },
+      "thu": {
+        "08-09": { "class_id": "lt204", "slotdata": "thu", "teacher_ID": 0 },
+        "09-10": { "class_id": "lt204", "slotdata": "thu", "teacher_ID": 0 },
+        "10-11": { "class_id": "lt204", "slotdata": "thu", "teacher_ID": 0 },
+        "11-12": { "class_id": "lt204", "slotdata": "thu", "teacher_ID": 0 },
+        "12-01": { "class_id": "lt204", "slotdata": "thu", "teacher_ID": 0 },
+        "01-02": { "class_id": "lt204", "slotdata": "thu", "teacher_ID": 0 },
+        "02-03": { "class_id": "lt204", "slotdata": "thu", "teacher_ID": 0 },
+        "03-04": { "class_id": "lt204", "slotdata": "thu", "teacher_ID": 0 },
+        "04-05": { "class_id": "lt204", "slotdata": "thu", "teacher_ID": 0 },
+        "05-06": { "class_id": "lt204", "slotdata": "thu", "teacher_ID": 0 }
+      },
+      "fri": {
+        "08-09": { "class_id": "lt204", "slotdata": "fri", "teacher_ID": 0 },
+        "09-10": { "class_id": "lt204", "slotdata": "fri", "teacher_ID": 0 },
+        "10-11": { "class_id": "lt204", "slotdata": "fri", "teacher_ID": 0 },
+        "11-12": { "class_id": "lt204", "slotdata": "fri", "teacher_ID": 0 },
+        "12-01": { "class_id": "lt204", "slotdata": "fri", "teacher_ID": 0 },
+        "01-02": { "class_id": "lt204", "slotdata": "fri", "teacher_ID": 0 },
+        "02-03": { "class_id": "lt204", "slotdata": "fri", "teacher_ID": 0 },
+        "03-04": { "class_id": "lt204", "slotdata": "fri", "teacher_ID": 0 },
+        "04-05": { "class_id": "lt204", "slotdata": "fri", "teacher_ID": 0 },
+        "05-06": { "class_id": "lt204", "slotdata": "fri", "teacher_ID": 0 }
+      },
+      "sat": {
+        "08-09": { "class_id": "lt204", "slotdata": "sat", "teacher_ID": 0 },
+        "09-10": { "class_id": "lt204", "slotdata": "sat", "teacher_ID": 0 },
+        "10-11": { "class_id": "lt204", "slotdata": "sat", "teacher_ID": 0 },
+        "11-12": { "class_id": "lt204", "slotdata": "sat", "teacher_ID": 0 },
+        "12-01": { "class_id": "lt204", "slotdata": "sat", "teacher_ID": 0 },
+        "01-02": { "class_id": "lt204", "slotdata": "sat", "teacher_ID": 0 },
+        "02-03": { "class_id": "lt204", "slotdata": "sat", "teacher_ID": 0 },
+        "03-04": { "class_id": "lt204", "slotdata": "sat", "teacher_ID": 0 },
+        "04-05": { "class_id": "lt204", "slotdata": "sat", "teacher_ID": 0 },
+        "05-06": { "class_id": "lt204", "slotdata": "sat", "teacher_ID": 0 }
+      },
+      "sun": {
+        "08-09": { "class_id": "lt204", "slotdata": "sun", "teacher_ID": 0 },
+        "09-10": { "class_id": "lt204", "slotdata": "sun", "teacher_ID": 0 },
+        "10-11": { "class_id": "lt204", "slotdata": "sun", "teacher_ID": 0 },
+        "11-12": { "class_id": "lt204", "slotdata": "sun", "teacher_ID": 0 },
+        "12-01": { "class_id": "lt204", "slotdata": "sun", "teacher_ID": 0 },
+        "01-02": { "class_id": "lt204", "slotdata": "sun", "teacher_ID": 0 },
+        "02-03": { "class_id": "lt204", "slotdata": "sun", "teacher_ID": 0 },
+        "03-04": { "class_id": "lt204", "slotdata": "sun", "teacher_ID": 0 },
+        "04-05": { "class_id": "lt204", "slotdata": "sun", "teacher_ID": 0 },
+        "05-06": { "class_id": "lt204", "slotdata": "sun", "teacher_ID": 0 }
+      }
+    }
+  }
 
-document.getElementById('go').addEventListener('click', () => {
-    let course = document.getElementById("course_option").value;
-    let semester = document.getElementById("semester_option").value;
-    let section = document.getElementById("section_option").value.toLowerCase();
+document.getElementById('letmesee').addEventListener('click',()=>{
+    for(var i=1;i<=7;i++){
+        let currrow = document.getElementById("mytable").rows[i].cells[0].innerHTML.toLowerCase();
+        for(var j=1;j<=10;j++){
+            let currcol = document.getElementById("mytable").rows[0].cells[j].innerHTML.toLowerCase();
+            document.getElementById("mytable").rows[i].cells[j].innerHTML = temp_obj.schedule[currrow][currcol].slotdata
+            if(!document.getElementById("mytable").rows[i].cells[j].innerHTML){
+                document.getElementById("mytable").rows[i].cells[j].setAttribute("class", "text bg-success");
+            }
+            else{
+                document.getElementById("mytable").rows[i].cells[j].setAttribute("class", "text bg-danger");
+            }
+        }
+    }
+})
+
+
+
+
+// document.getElementById('go').addEventListener('click', () => {
+//     let course = document.getElementById("course_option").value;
+//     let semester = document.getElementById("semester_option").value;
+//     let section = document.getElementById("section_option").value.toLowerCase();
     
-    let query = { "course":course, "semester":semester, "section":section } 
-    console.log(a);
+//     let query = { "course":course, "semester":semester, "section":section } 
+//     console.log(a);
 
-    fetch('http://127.0.0.1:3000/getCoordinates', {
-        method: 'POST',
-        headers:{
-            'Content-Type': 'application/json'
-        },
-        body: query
-    })
-    .then(response => response.json())
-    .then(data => {
-        points = data;
-        console.log(points); // Coordinates received from the server
-    })
-    .catch(error => console.error('out of service.. ~_~  @_@ (O 3 O):', error));
-});
-
-
-
-
-
-
-document.getElementById('-1').addEventListener('click', () => {
-    fetch('./mapgeoJSON/floor-1.geojson').then(response =>  response.json()).then(data => {
-        L.geoJSON(data, {
-            style:{color: 'cadetblue',weight: 1,opacity: 1},
-        }).addTo(map);
-    }).catch(error => console.error('out of service.. ~_~  @_@', error));   
-});
-
+//     fetch('http://127.0.0.1:3000/getCoordinates', {
+//         method: 'POST',
+//         headers:{
+//             'Content-Type': 'application/json'
+//         },
+//         body: query
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         points = data;
+//         console.log(points); // Coordinates received from the server
+//     })
+//     .catch(error => console.error('out of service.. ~_~  @_@ (O 3 O):', error));
+// });
+// document.getElementById('-1').addEventListener('click', () => {
+//     fetch('./mapgeoJSON/floor-1.geojson').then(response =>  response.json()).then(data => {
+//         L.geoJSON(data, {
+//             style:{color: 'cadetblue',weight: 1,opacity: 1},
+//         }).addTo(map);
+//     }).catch(error => console.error('out of service.. ~_~  @_@', error));   
+// });
