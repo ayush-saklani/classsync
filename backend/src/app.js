f3538d4959db0676600e04388d63cfb5dbc76470
RawatDevanshu <rawatdevanshu22@gmail.com>
2024-04-17 16:39:48 +0530

    added basic middlewares

---
+++ backend/src/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./.env" });
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "PATCH", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+export { app };
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,4 @@
-import express from "express";
-
-const app = express();
+import { app } from "./app.js";
 
 app.get("/", (req, res) => {
   res.send("hello world");
