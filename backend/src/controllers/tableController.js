d80ab08d08dce7b91310b18a0fe60f743894203f
RawatDevanshu <rawatdevanshu22@gmail.com>
2024-04-24 21:24:21 +0530

    functional api for testing

--- backend/src/controllers/tableController.js
+++ backend/src/controllers/tableController.js
@@ -1,9 +1,12 @@
+import data from "../assets/structured_timetable.json" assert { type: "json" };
 const get_timetable = async (req, res, next) => {
   console.log(req.query);
-  res.status(200).json({
-    status: "success",
-    message: "fetched tt",
-  });
+  const course_name = req.query.course;
+  const semester = req.query.semester;
+  const section = req.query.section;
+
+  const time_table = data.courses[course_name][semester][section];
+  res.status(200).json(time_table);
 };
 
 export { get_timetable };
