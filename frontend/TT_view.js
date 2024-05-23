fb285689542cb6fd65be34cea5d54ba2b452e840
“ayush <ayushsaklani04@gmail.com>
2024-05-23 15:20:10 +0530

    waring floating labels introduced

--- frontend/TT_view.js
+++ frontend/TT_view.js
@@ -21,6 +21,49 @@ let events = {
 	"2024-06-07": { "description": "2024-06-07 event testing adipisicing elit Pariatur similique" },
 	"2024-06-08": { "description": "2024-06-08 event testing adipisicing elit Pariatur similique" }
 };  
+let messageCounter = 0;
+const float_error_card_func = (title, desc, color) => {
+	const uniqueId = `float_error_card_${messageCounter++}`;
+
+	let div = document.createElement('div');
+	div.className = `card text-bg-${color} position-fixed bottom-0 end-0 m-3`;
+	div.style.maxWidth = "25rem";
+	div.id = uniqueId;
+
+	let headerDiv = document.createElement('div');
+	headerDiv.className = "card-header fw-bold";
+	div.appendChild(headerDiv);
+
+	let bodyDiv = document.createElement('div');
+	bodyDiv.className = "card-body";
+
+	let h5 = document.createElement('h5');
+	h5.className = "card-title";
+	h5.textContent = title;
+	bodyDiv.appendChild(h5);
+
+	let p = document.createElement('p');
+	p.className = "card-text";
+	p.textContent = desc;
+	bodyDiv.appendChild(p);
+
+	div.appendChild(bodyDiv);
+	document.getElementById('message_container').appendChild(div);
+
+	if (color === "success") {
+		headerDiv.innerHTML = `Success <i class="bi bi-check-circle-fill"></i>`;
+	} else if (color === "danger") {
+		headerDiv.innerHTML = `Warning <i class="bi bi-exclamation-triangle-fill"></i>`;
+	}
+	div.classList.add('rise');
+	setTimeout(() => {
+		div.classList.remove('rise');
+		div.classList.add('sink');
+		setTimeout(() => {
+			div.remove();
+		}, 1000);
+	}, 5000);
+}
 const letmesee2 = (temp_tt) => {
 	// main timetable rendering function
 	for (let i = 1; i <= 7; i++) {
@@ -111,6 +154,7 @@ const letmesee2 = (temp_tt) => {
 		}
 	}
 	else{
+		float_error_card_func("Timetable not found", "The timetable you are looking for is not available. Please try again later or contact admin.", "danger");
 		//empty the table and color the cells
 		let temp_teachertable = document.getElementById("teacher_table").getElementsByTagName('tbody')[0];
 		temp_teachertable.innerHTML = "";	
@@ -199,7 +243,10 @@ const letmeseeitbaby = () => {
 				college_event_manager();
 			}
 		})
-		.catch(error => console.error('Data unavailable:', error));
+		.catch(error => {
+            float_error_card_func("Timetable not found", "The timetable you are looking for is not found. Please try again later.", "danger");
+			console.error('Data unavailable:', error)
+		});
 }
 letmeseeitbaby();
 // document.getElementById('letmesee').addEventListener('click', letmeseeitbaby);
