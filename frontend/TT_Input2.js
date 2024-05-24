ae039cfb6b0b8fa172643fd16ebd28acdde07ad0
“ayush <ayushsaklani04@gmail.com>
2024-05-24 14:18:38 +0530

    bugfix

--- frontend/TT_Input2.js
+++ frontend/TT_Input2.js
@@ -7,7 +7,7 @@ document.getElementById("save_tt_json").addEventListener("click", save_table_fun
 // document.getElementById("showit").addEventListener("click",fetch_timetable); // button is not needed for now below change listner are used instead
 
 // [ Save on DB button ] teacher subject relation table ka listner
-document.getElementById("save_sub_teacher_table").addEventListener("click", pass_second_table_to_first);
+// document.getElementById("save_sub_teacher_table").addEventListener("click", pass_second_table_to_first);
 
 //testing button to execute function on the top of page 
 document.getElementById('course_option').addEventListener('change', fetch_timetable);
