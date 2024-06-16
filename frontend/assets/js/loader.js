ae36d6e4180e7fd10766049f3022f2d51a11e1a9
“ayush <ayushsaklani04@gmail.com>
2024-06-17 01:46:42 +0530

    loader fix

--- frontend/assets/js/loader.js
+++ frontend/assets/js/loader.js
@@ -1,14 +1,15 @@
-let mainheading = document.createElement('h1');
-mainheading.classList.add('mx-3', 'my-3', 'heading-text');
-mainheading.textContent = 'Class-Sync Timetable Manager';
-mainheading.style.zIndex = '1000';
-let delay_warning = document.createElement('h5');
-delay_warning.classList.add('mx-3', 'my-3', 'heading-text');
-delay_warning.style.display = 'none';
-delay_warning.textContent = `Server is probably doing a cold Starting, Please Wait...`;
-delay_warning.style.zIndex = '1000';
-document.getElementById("loader").appendChild(mainheading);
-document.getElementById("loader").appendChild(delay_warning);
+const loader_loading = ()=>{
+    let mainheading = document.createElement('h1');
+    mainheading.classList.add('mx-3', 'my-3', 'heading-text');
+    mainheading.innerHTML = 'Class-Sync Timetable Manager';
+    document.getElementById("loader").appendChild(mainheading);
+    let delay_warning = document.createElement('h5');
+    delay_warning.classList.add('mx-3', 'my-3', 'heading-text');
+    delay_warning.style.display = 'none';
+    delay_warning.innerHTML = `Server is probably doing a cold Starting, Please Wait...`;
+    document.getElementById("loader").appendChild(delay_warning);
+}
+loader_loading();
 setTimeout(() => {
-    delay_warning.style.display = '';
-}, 5000);
+    document.getElementById("loader").childNodes[1].style.display = 'flex';
+}, 6000);
