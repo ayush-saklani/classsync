f44f567d89e72508733967ca0b4e029c62ad1606
“ayush <ayushsaklani04@gmail.com>
2024-06-20 17:14:45 +0530

    page for resetting the whole database

---
+++ frontend/edit/reset/reset-database.js
@@ -0,0 +1,21 @@
+let faculty_data;
+let messageCounter = 0;
+document.getElementById('loader').style.display = 'none';
+
+const reset_faculty_data_button = ()=>{
+	
+}
+const reset_room_occupancy_data_button = ()=>{
+	
+}
+const remove_timetable_data_button = ()=>{
+	
+}
+const remove_all_faculty_button = ()=>{
+	
+}
+
+document.getElementById('reset_faculty_data_button').addEventListener('click', reset_faculty_data_button);
+document.getElementById('reset_room_occupancy_data_button').addEventListener('click', reset_room_occupancy_data_button);
+document.getElementById('remove_timetable_data_button').addEventListener('click', remove_timetable_data_button);
+document.getElementById('remove_all_faculty_button').addEventListener('click', remove_all_faculty_button);
