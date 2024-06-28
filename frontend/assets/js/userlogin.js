1ca2dd7ede81601a3df61315efddd75da268234b
“ayush <ayushsaklani04@gmail.com>
2024-06-28 21:39:01 +0530

    code improv + tester

--- frontend/assets/js/userlogin.js
+++ frontend/assets/js/userlogin.js
@@ -3,19 +3,24 @@ const delete_cookie = () => {       // logout function
     document.cookie = "uuid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     window.location.reload();
 }
-if (document.cookie.includes("uuid=")) {
-    // if(uuid is NOT correct then){
-    //     document.cookie = "uuid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-    //     window.location = "/login/";
-    // }
-    //  else 
-    let butt = document.createElement("button");
-    butt.className = "fw-bold h4 px-4 btn btn-lg btn-danger rounded-pill";
-    butt.id = "logout_button";
-    butt.innerHTML = "Logout";
-    butt.addEventListener("click", delete_cookie);
-    document.getElementById("login_button").replaceWith(butt);
+const validate_cookie = () => {
+    if (document.cookie.includes("uuid=")) {
+        // if(uuid is NOT correct then){
+        //     document.cookie = "uuid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        //     window.location = "/login/";
+        // }
+        //  else 
+
+        let butt = document.createElement("button");
+
+        butt.className = "fw-bold h4 px-4 btn btn-lg btn-danger rounded-pill";
+        butt.id = "logout_button";
+        butt.innerHTML = "Logout";
+        butt.addEventListener("click", delete_cookie);
+        document.getElementById("login_button").replaceWith(butt);
+    }
+    else {
+        window.location = "/login/";
+    }
 }
-else {
-    window.location = "/login/"; 
-}
+validate_cookie();
