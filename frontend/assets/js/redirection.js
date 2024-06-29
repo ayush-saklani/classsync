82172bf69a8d3228c4b817fa50121df1d51e599c
“ayush <ayushsaklani04@gmail.com>
2024-06-29 18:05:32 +0530

    redirection bug solved bug

--- frontend/assets/js/redirection.js
+++ frontend/assets/js/redirection.js
@@ -1,7 +1,7 @@
 // this redirect the pages acording to the domain name and the path
 let fileName = location.href.split("/")
 console.log(fileName)
-if (fileName[2] && fileName[2] == "gehutimetable.vercel.app") {
+if (fileName.includes("gehutimetable.vercel.app")) {
     if(fileName.length <= 4){
         window.location.href = "/view/";        // redirecting to view page automatically
     }
@@ -9,7 +9,7 @@ if (fileName[2] && fileName[2] == "gehutimetable.vercel.app") {
         window.location.href = "/view/";        // redirecting to edit page automatically
     }    
 }
-else if (fileName == "classsync.vercel.app") {
+else if (fileName.includes("classsync.vercel.app")) {
     if(fileName.length <= 4){
         window.location.href = "/edit/";        // redirecting to view page automatically
     }
