22f30fc795c4aa4bc303c3ae0256e3301968195a
“ayush <ayushsaklani04@gmail.com>
2024-06-29 18:00:13 +0530

    redirection bug solved  tester

--- frontend/assets/js/redirection.js
+++ frontend/assets/js/redirection.js
@@ -2,18 +2,18 @@
 let fileName = location.href.split("/")
 console.log(fileName)
 if (fileName[2] && fileName[2] == "gehutimetable.vercel.app") {
-    if(fileName.length <= 3){
+    if(fileName.length <= 4){
         window.location.href = "/view/";        // redirecting to view page automatically
     }
-    else if(fileName[3] && fileName[3] == "edit"){
+    else if(fileName.includes("edit")){
         window.location.href = "/view/";        // redirecting to edit page automatically
     }    
 }
 else if (fileName == "classsync.vercel.app") {
-    if(fileName.length <= 3){
+    if(fileName.length <= 4){
         window.location.href = "/edit/";        // redirecting to view page automatically
     }
-    else if(fileName[3] && fileName[3] == "view"){
+    else if(fileName.includes("view")){
         // window.location.href = "/edit/";        // redirecting to edit page automatically
     }
 }
