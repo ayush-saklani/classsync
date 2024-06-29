1c6897f55ef64f9507a594f0cabd39bdad518655
“ayush <ayushsaklani04@gmail.com>
2024-06-29 19:20:00 +0530

    login solved and bug removed admin is pass and uid for testing

--- frontend/assets/js/userlogin.js
+++ frontend/assets/js/userlogin.js
@@ -1,17 +1,17 @@
 // window.location = "/view/";
 const delete_cookie = () => {       // logout function
-    document.cookie = "uuid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/edit;";
+    document.cookie = "uuid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/edit ;";
     window.location.reload();
 }
 const validate_cookie = (sessionId) => {
-    document.cookie = "uuid=2117421; path=/edit;";                               //temp remove this line
-    let butt = document.createElement("button");                             //temp remove this line
-    butt.className = "fw-bold h4 px-4 btn btn-lg btn-danger rounded-pill";   //temp remove this line
-    butt.id = "logout_button";                                               //temp remove this line
-    butt.innerHTML = "Logout";                                               //temp remove this line
-    butt.addEventListener("click", delete_cookie);                           //temp remove this line
-    document.getElementById("login_button").replaceWith(butt);               //temp remove this line
-
+    let butt = document.createElement("button");                             /////////////////////  temp remove this line
+    butt.className = "fw-bold h4 px-4 btn btn-lg btn-danger rounded-pill";   /////////////////////  temp remove this line
+    butt.id = "logout_button";                                               /////////////////////  temp remove this line
+    butt.innerHTML = "Logout";                                               /////////////////////  temp remove this line
+    butt.addEventListener("click", delete_cookie);                           /////////////////////  temp remove this line
+    document.getElementById("login_button").replaceWith(butt);               /////////////////////  temp remove this line
+    return                                                                   /////////////////////  temp remove this line           
+       
     fetch('http://localhost:3000/logindata/login_uuid_validate', {
         method: 'POST',
         headers: {
@@ -29,57 +29,23 @@ const validate_cookie = (sessionId) => {
                 document.getElementById("login_button").replaceWith(butt);
 
                 console.log('Session is valid' + response.json());      // Session is valid, continue with user interactions
+                float_error_card_function("Session is valid", "bg-success");
             }
             else {
+                document.cookie = "uuid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/edit ;";   //uncomment this line
+                window.location.href = '/login';                                             //uncomment this line
                 throw new Error('Session validation failed');   // Session is invalid, handle accordingly (e.g., redirect to login)
             }
         })
         .catch(error => {
             console.error('Error:', error);
-            // document.cookie = "uuid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/edit;";   //uncomment this line
-            // window.location.href = '/login';                                             //uncomment this line
         });
 }
-const login = () => {
+document.addEventListener('DOMContentLoaded', ()=>{
     if (document.cookie.includes("uuid=")) {
         validate_cookie(document.cookie.split("=")[1]);
-        return;
     }
-    else {
-        document.cookie = "uuid=2117421; path=/edit;";      //temp remove this line
-        fetch('http://localhost:3000/logindata/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ uid: 'user123', password: 'password123' })
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Invalid UUID');
-                }
-                document.cookie = `uuid=${response.json().uuid};path=/edit;`;
-                return response.json();
-            })
-            .then(data => {
-                document.cookie = `uuid=${data.uuid};path=/edit;`;
-                if (data.valid) {
-                    let butt = document.createElement("button");
-                    butt.className = "fw-bold h4 px-4 btn btn-lg btn-danger rounded-pill";
-                    butt.id = "logout_button";
-                    butt.innerHTML = "Logout";
-                    butt.addEventListener("click", delete_cookie);
-                    document.getElementById("login_button").replaceWith(butt);
-                } else {
-                    document.cookie = "uuid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/edit;";
-                    window.location = "/login/";
-                }
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                document.cookie = "uuid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/edit;";
-                window.location = "/login/";
-            });
+    else{
+        window.location.href = '/login';
     }
-}
-document.addEventListener('DOMContentLoaded', login);
+});
---
+++ frontend/login/loginhandler.js
@@ -0,0 +1,59 @@
+// window.location = "/view/";
+// document.getElementById("signin_error").style.display = "none";
+let messageCounter = 0;
+let display_error = (message) => {  // wrong username or password
+    document.getElementById("signin_email").style.border = "2px solid #dc3545";
+    document.getElementById("signin_InputPassword").style.border = "2px solid rgba(1, 106, 137, 0.827)";
+    let error = document.getElementById("signin_error");
+    error.innerHTML = (message || "Wrong username or password") + " *";
+}
+let reverse_display_error = (message) => {  // wrong username or password
+    document.getElementById("signin_email").style.border = ""
+    document.getElementById("signin_InputPassword").style.border = "";
+    let error = document.getElementById("signin_error");
+    error.innerHTML = "";
+}
+const login = () => {
+    let email = document.getElementById("signin_email").value;             
+    let password = document.getElementById("signin_InputPassword").value;  
+
+    if(email == "admin" && password == "admin"){
+        document.cookie = "uuid=2117421; path=/edit ;";             //////////////temp remove this line
+        float_error_card_func("Login Successful", "", "success");   //////////////temp remove this line
+        setInterval(() => {                                         //////////////temp remove this line
+            window.location.href = '/edit';                         //////////////temp remove this line
+        }, 2000);                                                   //////////////temp remove this line
+        return                                                      //////////////temp remove this line
+    }else{                                                          //////////////temp remove this line                                   
+        display_error("Email or password is incorrect");            //////////////temp remove this line
+        return                                                      //////////////temp remove this line
+    }
+
+    fetch('http://localhost:3000/logindata/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ uid: email, password: password })
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Invalid UUID');
+            }
+            else {
+                return response.json();
+            }
+        })
+        .then(data => {
+            document.cookie = `uuid=${data.data.uid};path=/edit ;`;
+            float_error_card_func("Login Successful", "", "success");
+            document.cookie = `uuid=${data.uuid};path=/edit ;`;
+        })
+        .catch(error => {
+            display_error("Email or password is incorrect");
+            console.error('Error:', error);
+        });
+}
+document.getElementById("login").addEventListener("click", login);
+document.getElementById("signin_email").addEventListener("change", reverse_display_error);
+document.getElementById("signin_InputPassword").addEventListener("change", reverse_display_error);
