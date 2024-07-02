5b7a58c5ef72b65292918ad74f5f46a06573962e
“ayush <ayushsaklani04@gmail.com>
2024-07-02 23:17:44 +0530

    BACKEND ( CHANGE OF DOMAIN) !!

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -74,6 +74,7 @@ const loginUser = asyncHandler(async (req, res, next) => {
     const options = {
         httpOnly: true,
         secure: true,
+        domain: req.hostname,
     };
 
     return res
