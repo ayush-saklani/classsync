326fcb648cad3ee4a276fe23ebe83246d96b0018
RawatDevanshu <rawatdevanshu22@gmail.com>
2024-07-06 17:16:21 +0530

    secured routes with middleware

--- backend/src/routes/subjecttable.js
+++ backend/src/routes/subjecttable.js
@@ -4,11 +4,14 @@ import {
     save_subjecttable,
     update_subjecttable,
 } from "../controllers/subjecttableController.js";
+import { verifyJWT } from "../middleware/authmiddleware.js";
 
 const router = Router();
 
 router.get("/get", get_subjecttable);
-router.post("/save", save_subjecttable);
-router.get("/update", update_subjecttable);
+
+// secured routes
+router.post("/save", verifyJWT, save_subjecttable);
+router.get("/update", verifyJWT, update_subjecttable);
 
 export default router;
