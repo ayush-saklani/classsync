3e9898186475e4938bf712c4135031d312d9fbad
“ayush <ayushsaklani04@gmail.com>
2024-07-07 17:02:04 +0530

    error card updated

--- frontend/assets/js/float_error_card_func.js
+++ frontend/assets/js/float_error_card_func.js
@@ -2,23 +2,51 @@
 
 const float_error_card_func = (title, desc, color) => {
 	const uniqueId = `float_error_card_${messageCounter++}`;
-
+	// example of the card
+	// <div class="card text-bg-danger bottom-0 end-0 me-3 mt-1 float-error-card" id="float_error_card_0"style="max-width: 20rem;">
+	// 	<div class="card-header fw-bold py-1">
+    //         <span>
+    //             <i class="bi bi-exclamation-triangle-fill h5"></i>
+    //             Message Title
+    //         </span>
+    //         <button class="btn float-end p-0 m-0 card-header" style="-webkit-text-stroke-width: 1px;">
+    //             <i class="bi bi-x-circle h5"></i>
+    //         </button>
+    //         <span class="btn float-end p-0 m-0 me-2 card-header fw-bold ps-2"> 1</span>
+    //     </div>
+    //     <div class="card-body py-2">
+    //         <p class="card-text">
+    //             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste, adipisci. lorem10
+    //         </p>
+    //     </div>
+    // </div>
 	let div = document.createElement('div');
 	div.className = `card text-bg-${color} bottom-0 end-0 me-3 mt-1 float-error-card`; // position-fixed
 	div.style.maxWidth = "25rem";
 	div.id = uniqueId;
 
 	let headerDiv = document.createElement('div');
-	headerDiv.className = "card-header fw-bold";
-	div.appendChild(headerDiv);
+	headerDiv.className = "card-header fw-bold py-1";
 
 	let headerText = document.createElement('span');
+	if (color === "success") {
+		headerText.innerHTML = `<i class="bi bi-check-circle-fill"></i> ${title}`;
+	} else if (color === "danger") {
+		headerText.innerHTML = `<i class="bi bi-exclamation-triangle-fill"></i> ${title}`;
+	} else if (color === "warning") {
+		headerText.innerHTML = `<i class="bi bi-exclamation-octagon-fill"></i> ${title}`;
+	} else if (color === "info") {
+		headerText.innerHTML = `<i class="bi bi-info-circle-fill"></i> ${title}`;
+	} else if (color === "primary") {
+		headerText.innerHTML = `<i class="bi bi-info-circle-fill"></i> ${title}`;
+	}
+	// headerText.innerHTML = `${title}`;
 	headerDiv.appendChild(headerText);
 
 	let dismissButton = document.createElement('button');
-	dismissButton.innerHTML = `<i class="bi bi-x-circle h5"></i>`;
-	dismissButton.style.webkitTextStrokeWidth = "1px";
 	dismissButton.className = "btn float-end p-0 m-0 card-header";
+	dismissButton.style.webkitTextStrokeWidth = "1px";
+	dismissButton.innerHTML = `<i class="bi bi-x-circle h5"></i>`;
 	dismissButton.addEventListener('click', () => {
 		div.classList.add('sink');
 		setTimeout(() => {
@@ -29,36 +57,26 @@ const float_error_card_func = (title, desc, color) => {
 
 	let messagenumber = document.createElement('span');
 	messagenumber.innerHTML = ` ${messageCounter} `;
-	messagenumber.className = "btn float-end p-0 m-0 me-2 card-header fw-bold";
+	messagenumber.className = "btn float-end p-0 m-0 me-2 card-header fw-bold ps-2";
 	headerDiv.appendChild(messagenumber);
 
-	let bodyDiv = document.createElement('div');
-	bodyDiv.className = "card-body";
+	div.appendChild(headerDiv);
 
-	let h5 = document.createElement('h5');
-	h5.className = "card-title";
-	h5.innerHTML = title;
-	bodyDiv.appendChild(h5);
+
+	let bodyDiv = document.createElement('div');
+	bodyDiv.className = "card-body py-2";
 
 	let p = document.createElement('p');
 	p.className = "card-text";
 	p.innerHTML = desc;
 	bodyDiv.appendChild(p);
 
-	div.appendChild(bodyDiv);
+	if(desc){
+		div.appendChild(bodyDiv);
+	}
+
 	document.getElementById('message_container').appendChild(div);
 
-	if (color === "success") {
-		headerText.innerHTML = `Success <i class="bi bi-check-circle-fill"></i>`;
-	} else if (color === "danger") {
-		headerText.innerHTML = `Warning <i class="bi bi-exclamation-triangle-fill"></i>`;
-	} else if (color === "warning") {
-		headerText.innerHTML = `Warning <i class="bi bi-exclamation-octagon-fill"></i>`;
-	} else if (color === "info") {
-		headerText.innerHTML = `Info <i class="bi bi-info-circle-fill"></i>`;
-	} else if (color === "primary") {
-		headerText.innerHTML = `Info <i class="bi bi-info-circle-fill"></i>`;
-	}
 	div.classList.add('rise');
 	setTimeout(() => {
 		div.classList.remove('rise');
@@ -68,28 +86,29 @@ const float_error_card_func = (title, desc, color) => {
 		}, 1000);
 	}, 7000);
 }
-document.addEventListener('mousemove', function(event) {
-    const thresholdw = 100; // Adjusted for more reasonable corner detection
-    const thresholdh = 1000; // Adjusted for more reasonable corner detection
-    const windowWidth = window.innerWidth;
-    const windowHeight = window.innerHeight;
+// this shift the message container to the other side if mouse cursor is near the corner
+// document.addEventListener('mousemove', function(event) {
+//     const thresholdw = 100; // Adjusted for more reasonable corner detection
+//     const thresholdh = 1000; // Adjusted for more reasonable corner detection
+//     const windowWidth = window.innerWidth;
+//     const windowHeight = window.innerHeight;
     
-    const bottomRightArea = {
-        x: windowWidth - thresholdw,
-        y: windowHeight - thresholdh
-    };
-    const bottomLeftArea = {
-        x: thresholdw,
-        y: windowHeight - thresholdh
-    };
+//     const bottomRightArea = {
+//         x: windowWidth - thresholdw,
+//         y: windowHeight - thresholdh
+//     };
+//     const bottomLeftArea = {
+//         x: thresholdw,
+//         y: windowHeight - thresholdh
+//     };
 
-    const messageContainer = document.getElementById('message_container');
+//     const messageContainer = document.getElementById('message_container');
 
-    if (event.clientX > bottomRightArea.x && event.clientY > bottomRightArea.y) {
-        messageContainer.style.left = '0';
-        messageContainer.style.right = 'auto';
-    } else if (event.clientX < bottomLeftArea.x && event.clientY > bottomLeftArea.y) {
-        messageContainer.style.right = '0';
-        messageContainer.style.left = 'auto';
-    }
-});
+//     if (event.clientX > bottomRightArea.x && event.clientY > bottomRightArea.y) {
+//         messageContainer.style.left = '0';
+//         messageContainer.style.right = 'auto';
+//     } else if (event.clientX < bottomLeftArea.x && event.clientY > bottomLeftArea.y) {
+//         messageContainer.style.right = '0';
+//         messageContainer.style.left = 'auto';
+//     }
+// });
