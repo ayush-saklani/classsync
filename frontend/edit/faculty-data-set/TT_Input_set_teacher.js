28371641e456797262bb92173e9cb502afffb12f
“ayush <ayushsaklani04@gmail.com>
2024-08-01 20:00:38 +0530

    change teacher mid way api hookup

--- frontend/edit/faculty-data-set/TT_Input_set_teacher.js
+++ frontend/edit/faculty-data-set/TT_Input_set_teacher.js
@@ -91,6 +91,38 @@ const save_table_func = () => {                         //  function below calcu
             console.error('::::: FACULTY ERROR SAVING DATA :::::', error);
         });
 }
+const removeTeacherFromThisSection = (teacherid, course, semester, section, subjectcode) => {  //  this function removes the teacher from the section
+    return new Promise((resolve,reject) => {
+        try{
+            return fetch(`${localhost}/faculty/reset?` + new URLSearchParams({ course: course, semester: semester, section: section, teacherid: teacherid}), {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${getCookie('accessToken')}`
+                },
+                credentials: 'include'
+            }).then(response => {
+                if(response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error(':::::  FACULTY DATA NOT SAVED DUE TO NETWORK ERROR :::::');
+                }
+            }).then(data => {
+                console.log(data);
+                float_error_card_func(data.data, "", "success");
+                initializePage();
+                resolve(data); 
+                document.getElementById("save_tt_json").disabled = false;
+            }).catch(error => {
+                float_error_card_func("Teacher removed", "", "danger");
+                console.error('Data unavailable:', error)
+            });
+        }
+        catch(error){
+            reject(error);
+        }
+    });
+}
 const render_tables = () => {                           // renders the table [ uses the same strucute of JSON as it POST to the backend]
     if (timetable) {
         float_error_card_func("Section Data Available", "", "success");
@@ -154,39 +186,37 @@ const render_tables = () => {                           // renders the table [ u
             
             cell = newRow.insertCell();
             cell_insert = document.createElement("button");
-            cell_insert.innerHTML = "Reset";
+            cell_insert.innerHTML = "Remove Teacher";
             cell_insert.addEventListener("click", function () {
                 let row = this.parentNode.parentNode;
-                if(row.cells[1].childNodes[0].value != "0") {
-                // if(row.cells[1].childNodes[0].value != "0" && row.cells[1].childNodes[0].disabled == true) {
-                    // if(window.confirm("Are you sure you want to reset the teacher for this subject?")){
-                        console.log(document.getElementById("course_option").value);
-                        console.log(document.getElementById("semester_option").value);
-                        console.log(document.getElementById("section_option").value);
-                        console.log(row.cells[1].childNodes[0].value);
-                        console.log(row.cells[2].childNodes[0].innerHTML);
-                        let days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
-                        let currcol = ["08-09", "09-10", "10-11", "11-12", "12-01", "01-02", "02-03", "03-04", "04-05", "05-06"];
-                        for (let i = 1; i <= 7; i++) {
-                            let currrow = days[i - 1];
-                            for (let j = 1; j <= 10; j++) {
-                                let tempdayslot = currcol[j - 1];
-                                if (timetable.schedule[currrow][tempdayslot].subjectcode == row.cells[2].childNodes[0].innerHTML){
-                                    console.log(timetable.schedule[currrow][tempdayslot].subjectcode == row.cells[2].childNodes[0].innerHTML);                                 
-                                    console.log(timetable.schedule[currrow][tempdayslot].class_id);                                 
-                                }                                 
-                            }
-                        }
-                        // course, semester, section, teacherid, subjectcode
-                        // api call to reset the particular teacher 
-                        // api call to reset the particular section timetable 
-                        // api call to reset the particular room timetable 
-                    // }
+                if(row.cells[1].childNodes[0].value != "0" && row.cells[1].childNodes[0].disabled == true) {
+                    if(window.confirm("Are you sure you want to reset the teacher for this subject?")){
+                        let teacherid = row.cells[1].childNodes[0].value;
+                        let course = document.getElementById("course_option").value;
+                        let semester = document.getElementById("semester_option").value;
+                        let section = document.getElementById("section_option").value;
+                        let subjectcode = row.cells[2].childNodes[0].innerHTML;
+                        console.log({
+                            "teacherid " : teacherid,
+                            "course " : course,
+                            "semester " : semester,
+                            "section " : section,
+                            "subjectcode " : subjectcode,
+                        });
+                        removeTeacherFromThisSection(teacherid, course, semester, section, subjectcode);
+                    }
                 }                
             });
-            cell_insert.setAttribute("class", "btn bg-theory fw-bolder rounded-pill");
+            cell_insert.setAttribute("class", "btn btn-danger fw-bolder rounded-pill text");
+            cell_insert.setAttribute("style", "box-shadow: 1px 1px 7px  #000000;transition: 0.2s;");
+            cell_insert.addEventListener("mouseover", function () {
+                this.style.transform = "scale(1.1)";
+            });
+            cell_insert.addEventListener("mouseout", function () {
+                this.style.transform = "scale(1.0)";
+            });
             cell.appendChild(cell_insert);
-            cell.setAttribute("class", "border-dark border-3 p-1 align-middle ");
+            cell.setAttribute("class", "border-dark border-3 py-1 mx-2 text-center");
         }
     }
     else {
@@ -304,7 +334,7 @@ document.getElementById('course_option').addEventListener('change', initializePa
 document.getElementById('semester_option').addEventListener('change', initializePage);  // [ semester select box eventlistner ]
 document.getElementById('section_option').addEventListener('change', initializePage);	// [ section select box eventlistner ]
 document.addEventListener('DOMContentLoaded', () => {                                     //  this function initializes the page
-    document.getElementById('semester_option').value = "3";
+    document.getElementById('semester_option').value = "1";
     addDynamicSectionOptions();
     initializePage();                       // initialize the page by fetching the room list, faculty list and timetable data from the server
 });
