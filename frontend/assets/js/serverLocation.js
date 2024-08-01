e427ff05f8a0a92a93d2bc42f2b888470803c012
“ayush <ayushsaklani04@gmail.com>
2024-08-01 13:02:18 +0530

    ui improv

--- frontend/assets/js/serverLocation.js
+++ frontend/assets/js/serverLocation.js
@@ -1,2 +1,2 @@
-const localhost = "https://classsync-3ht1.onrender.com";
-// const localhost = "http://localhost:3000";
+// const localhost = "https://classsync-3ht1.onrender.com";
+const localhost = "http://localhost:3000";
