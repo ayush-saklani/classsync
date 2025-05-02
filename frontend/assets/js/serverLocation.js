1b5fa357f9a936c43252be4547e16f6ff081cbca
ayush saklani <ayushsaklani04@gmail.com>
2025-05-02 13:28:03 +0530

    updates

--- frontend/assets/js/serverLocation.js
+++ frontend/assets/js/serverLocation.js
@@ -1,3 +1,3 @@
-// const localhost = "https://classsync-3ht1.onrender.com";
-const localhost = "https://class-sync-azure.azurewebsites.net";
+const localhost = "https://classsync-3ht1.onrender.com";
+// const localhost = "https://class-sync-azure.azurewebsites.net";
 // const localhost = "http://localhost:3000";
