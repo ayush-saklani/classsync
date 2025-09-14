a0d09863c13c71ed7a49695f1474fdd8e9b391af
ayush saklani <ayushsaklani04@gmail.com>
2025-09-14 21:48:38 +0530

    updates

--- frontend-react/src/app/view/students/page.tsx
+++ frontend-react/src/app/view/students/page.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import React from 'react';
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import DynamicOptions from "@/components/DynamicOptions";
