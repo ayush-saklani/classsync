a7df46159753ba500d84362ebc5366f33a230d05
ayush saklani <ayushsaklani04@gmail.com>
2025-09-15 13:58:00 +0530

    day 2 convertion

--- frontend-react/src/app/edit/room-data-editor/room-editor/page.tsx
+++ frontend-react/src/app/edit/room-data-editor/room-editor/page.tsx
@@ -6,8 +6,8 @@ import Footer from "@/components/footer";
 import { room_schema } from '@/models/room.model';
 import toast from 'react-hot-toast';
 import { fetch_all_rooms, save_all_rooms } from '@/utils/fetchroom';
-import DynamicOptions from '@/components/DynamicOptions';
 import { RiResetRightFill } from 'react-icons/ri';
+import { room_type_options } from '@/utils/constant';
 
 export default function RoomPage() {
   const [room_list, setroom_list] = useState<room_schema[]>([]);
@@ -26,34 +26,21 @@ export default function RoomPage() {
     fetchData();
   }, []);
 
-  const [course, setCourse] = useState('');
-  const [semester, setSemester] = useState('');//they are needed no matter what
-  const [section, setSection] = useState('');//they are needed no matter what
-  useEffect(() => {
-    const fetchData = async () => {
-      let rooms = await fetch_all_rooms();
-      rooms.sort((a: { roomid: any; }, b: { roomid: any; }) => a.roomid.localeCompare(b.roomid));
-      setroom_list(rooms);
-    }
-    fetchData();
-  }, [course]);
   return (
     <>
       <Header />
       <div className="container-fluid mt-5">
         <div className="container">
-          <h1 className="center text fw-bold">Room Allotment Portal</h1>
+          <h1 className="center text fw-bold">Room Data Editing Portal</h1>
           <dl className="pt-3 mx-4">
             <h4 className="center text text-danger fw-bold">Warning <i className="bi bi-exclamation-triangle-fill"></i> </h4>
             <li className="center text text-danger fw-bold">
               Naming schema can be find <a href="/assets/doc/room-schema.pdf" target="_blank">here</a>
             </li>
+            <li className="center text text-danger fw-bold">Yellow color indicates that room is being used</li>
             <li className="center text text-danger fw-bold">Contact Developers for any query, help and support</li>
           </dl>
           <div className="">
-            <div className='col-11'>
-              <DynamicOptions course={course} setCourse={setCourse} semester={semester} setSemester={setSemester} section={section} setSection={setSection} onlyone={1} />
-            </div>
             <div className="align-items-center justify-content-center mx-3">
               <button
                 type="button"
@@ -79,7 +66,7 @@ export default function RoomPage() {
               <table className="table" id="room_table">
                 <thead>
                   <tr>
-                    <th className="table-light text border-dark border-2 col-2" scope="col">Room ID</th>
+                    <th className="table-light text border-dark border-2 col-1" scope="col">Room ID</th>
                     <th className="table-light text border-dark border-2 col-5" scope="col">Room Name</th>
                     <th className="table-light text border-dark border-2 col-2" scope="col">Capacity</th>
                     <th className="table-light text border-dark border-2 col-2" scope="col">Room Type</th>
@@ -88,41 +75,90 @@ export default function RoomPage() {
                 </thead>
                 <tbody>
                   {room_list.map((room: room_schema) => (
-                    <tr key={room.roomid} className={room.roomid === "0" ? " " : "bg-red-600"}>
-                      <td className={`font-bold text border-dark border-2 col-2 align-middle ${room.roomid === "0" ? " text-danger" : ""}`} scope="row">{room.roomid}</td>
-                      <td className={`font-bold text border-dark border-2 col-5 align-middle ${room.roomid === "0" ? " text-danger" : ""}`}>{room.name}</td>
-                      <td className={`font-bold text border-dark border-2 col-2 align-middle ${room.roomid === "0" ? " text-danger" : ""}`}>{room.capacity}</td>
-                      <td className={`font-bold text border-dark border-2 col-2 align-middle ${room.roomid === "0" ? " text-danger" : ""}`}>{room.type}</td>
-                      <td className={`p-0 font-bold text border-dark border-2 col-1 align-middle ${room.allowed_course?.includes(course) ? " bg-warning" : ""}`}>
-                        <div className="form-check form-switch d-flex justify-content-center align-items-center" style={{ height: "100%" }}>
+                    <tr key={room.roomid} className={room.roomid === "0" ? " " : ""}>
+                      <td
+                        className={`p-0 font-bold text border-dark border-2 col-1 align-middle text-center ${room.roomid === "0" ? " text-danger" : ""}`}
+                        scope="row"
+                      >
+                        {room.roomid === "0" ? (
+                          room.roomid
+                        ) : (
                           <input
-                            className={` form-check-input ${room.allowed_course?.includes(course) ? " border-warning" : ""}`}
-                            type="checkbox"
-                            role="switch"
-                            id={`flexSwitchCheckDefault_${room.roomid}`}
-                            checked={room.allowed_course?.includes(course)}
-                            onChange={() => {
+                            type="text"
+                            className="form-control font-bold text-center"
+                            value={room.roomid}
+                            onChange={e => {
+                              const newRoomId = e.target.value;
                               setroom_list(prev =>
                                 prev.map(r =>
-                                  r.roomid === room.roomid
-                                    ? {
-                                      ...r,
-                                      allowed_course: r.allowed_course?.includes(course)
-                                        ? r.allowed_course.filter(c => c !== course)
-                                        : [...(r.allowed_course || []), course]
-                                    }
-                                    : r
+                                  r.roomid === room.roomid ? { ...r, roomid: newRoomId } : r
                                 )
                               );
                             }}
-                            style={{
-                              width: "3rem",
-                              height: "1.5rem",
-                              cursor: "pointer",
+                          />
+                        )}
+                      </td>
+                      <td className={`p-0 font-bold text border-dark border-2 col-5 align-middle ${room.roomid === "0" ? " text-danger" : ""}`}>
+                        {room.roomid === "0" ? (
+                          room.name
+                        ) : (
+                          <input
+                            type="text"
+                            className="form-control font-bold"
+                            value={room.name}
+                            onChange={e => {
+                              const newName = e.target.value;
+                              setroom_list(prev =>
+                                prev.map(r =>
+                                  r.roomid === room.roomid ? { ...r, name: newName } : r
+                                )
+                              );
                             }}
                           />
-                          <label className="form-check-label ms-2" htmlFor={`flexSwitchCheckDefault_${room.roomid}`}></label>
-                        </div>
+                        )}
+                      </td>
+                      <select
+                        className={`form-select font-bold text border-dark border-0 col-2 align-middle ${room.roomid === "0" ? " text-danger" : ""}`}
+                        value={room.capacity}
+                        onChange={e => {
+                          const newCapacity = Number(e.target.value);
+                          setroom_list(prev =>
+                            prev.map(r =>
+                              r.roomid === room.roomid ? { ...r, capacity: newCapacity } : r
+                            )
+                          );
+                        }}
+                        disabled={room.roomid === "0"}
+                      >
+                        {[1, 2, 3, 4].map(option => (
+                          <option key={option} value={option}>
+                            {option}
+                          </option>
+                        ))}
+                      </select>
+                      <td className={`p-0 text border-dark border-2 col-2 ${room.roomid === "0" ? " text-danger" : ""}`}>
+                        <select
+                          className="form-select text border-0 "
+                          value={room.type}
+                          onChange={e => {
+                            const newType = e.target.value;
+                            setroom_list(prev =>
+                              prev.map(r =>
+                                r.roomid === room.roomid ? { ...r, type: newType } : r
+                              )
+                            );
+                          }}
+                          disabled={room.roomid === "0"}
+                        >
+                          {room_type_options.map(option => (
+                            <option key={option} value={option}>
+                              {option}
+                            </option>
+                          ))}
+                        </select>
+                      </td>
+                      <td className={`p-0 font-bold text border-dark border-2 col-1 align-middle ${room.allowed_course?.length > 0 ? " bg-warning" : ""}`}>
+                        {room.allowed_course.join(", ")}
                       </td>
                     </tr>
                   ))}
@@ -142,7 +178,7 @@ export default function RoomPage() {
               </div>
             </div>
           </div>
-        </div>
+        </div >
       )}
       <Footer />
     </>
--- frontend-react/src/utils/constant.tsx
+++ frontend-react/src/utils/constant.tsx
@@ -1,2 +1,4 @@
 export const localhost = "https://class-sync-azure.azurewebsites.net";
+export const room_type_options = ["class", "hall", "computerlab", "englishlab", "electroniclab", "mechanicallab", "civillab", "drawinglab", "physicslab", "chemistrylab", "ladieswashroom", "gentswashroom", "office", "staffroom", "other"];
+
 // const localhost = "http://localhost:3000";
